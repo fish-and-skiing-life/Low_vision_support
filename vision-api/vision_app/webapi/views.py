1324846a9e91c2762bde14c8c7b2e06a9ddf7600
yaelanya <asahi.hentona@gmail.com>
2020-08-23 18:59:21 +0900

    make model

--- vision-api/vision_app/webapi/views.py
+++ vision-api/vision_app/webapi/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from webapi.models import Profile
+from webapi.serializers import ProfileSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ProfileListCreate(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
