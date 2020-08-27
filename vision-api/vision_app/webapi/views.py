035d432dc4d54b8ee46980a36c7b77dbec58ff61
tetsu <tettyann0405@gmail.com>
2020-08-27 19:41:59 +0900

    [add]ff

--- vision-api/vision_app/webapi/views.py
+++ vision-api/vision_app/webapi/views.py
@@ -1,7 +1,10 @@
 from webapi.models import Profile
 from webapi.serializers import ProfileSerializer
 from rest_framework import generics
-
+from rest_framework import views
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .lib import crawling
 
 class ProfileListCreate(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
