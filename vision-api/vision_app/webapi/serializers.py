63958911b2bf4b94d22c6d5677ec2cc09c9b1719
corechika <rconctmi121341@gmail.com>
2020-08-29 16:50:46 +0900

    WIP add category extraction API

--- vision-api/vision_app/webapi/serializers.py
+++ vision-api/vision_app/webapi/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from webapi.models import Summarization
+from webapi.models import Summarization, ArticleCategory
 
 class SummarizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Summarization
         fields = ('media', 'article_id')
+
+class ArticleCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleCategory
+        fields = ('media')
