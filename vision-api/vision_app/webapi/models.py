5b435c7bd52d138a49cc93071767e2008012ed33
tetsu <tettyann0405@gmail.com>
2020-09-19 15:40:31 +0900

    [add] データベースカラム変更

--- vision-api/vision_app/webapi/models.py
+++ vision-api/vision_app/webapi/models.py
@@ -4,13 +4,13 @@ class Article(models.Model):
     url = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
     content = models.TextField(max_length=4000)
-    vector = models.FloatField()
+    vector = models.TextField(max_length=4000)
 
 
 class Named_entity(models.Model):
     url = models.CharField(max_length=256)
     word = models.TextField(max_length=256)
-    vector = models.FloatField()
+    vector = models.TextField(max_length=4000)
 
 
 class Trend(models.Model):
