54e5b6e9d6e216c8bce0e93c2fa47fad2b767ffd
yaelanya <asahi.hentona@gmail.com>
2020-09-22 22:39:25 +0900

    add article load method

--- vision-api/vision_app/webapi/lib/recommender.py
+++ vision-api/vision_app/webapi/lib/recommender.py
@@ -8,34 +8,7 @@ from ..models import Article
 
 class ArticleRecommender:
     def __init__(self):
-        query = """
-            SELECT url, title, vector 
-            FROM webapi_article
-            WHERE vector != ''
-        """
-        # 記事データの読み込み
-        article_df = pd.read_sql_query(query, connection)
-
-        # 記事ベクトルを文字列からnumpy.ndarrayに変換
-        article_df.loc[:, 'vector'] = article_df.apply(
-            lambda x: self._str2vec(x['vector'])
-            , axis=1
-        )
-
-        self.article_df = article_df.dropna()
-
-    def _str2vec(self, raw_vec):
-        # eval に失敗したらnp.nanを返す
-        try:
-            vec = eval(raw_vec)
-        except Exception:
-            return np.nan
-        
-        # listでなければnp.nanを返す
-        if not isinstance(vec, list):
-            return np.nan
-        
-        return np.array(vec)
+        self.article_df = self._load_articles()
 
     def get_similar_articles(self, article_url, article_vector, topn=5):
         # 読んでいる記事以外の記事の情報を取得する
@@ -54,6 +27,41 @@ class ArticleRecommender:
     def calc_similarities(self, target, others):
         similarities =  [scipy.spatial.distance.cosine(target, other) for other in others]
         return np.array(similarities)
+
+    def _load_articles(self):
+        query = """
+            SELECT url, title, vector 
+            FROM webapi_article
+            WHERE vector != ''
+        """
+        # 記事データの読み込み
+        article_df = pd.read_sql_query(query, connection)
+        # nanが入っているデータは削除
+        article_df.dropna(inplace=True)
+        # URLが重複している記事は削除
+        article_df.drop_duplicates(['body'], keep='last', inplace=True)
+        # 記事ベクトルを文字列からnumpy.ndarrayに変換
+        article_df.loc[:, 'vector'] = article_df.apply(
+            lambda x: self._str2vec(x['vector'])
+            , axis=1
+        )
+        # ベクトルに変換できなかったデータは削除
+        article_df.dropna(inplace=True)
+
+        return article_df
+
+    def _str2vec(self, raw_vec):
+        # eval に失敗したらnp.nanを返す
+        try:
+            vec = eval(raw_vec)
+        except Exception:
+            return np.nan
+        
+        # listでなければnp.nanを返す
+        if not isinstance(vec, list):
+            return np.nan
+        
+        return np.array(vec)
 
     def _get_vectors(self, df):
         return np.array(df['vector'].tolist())
